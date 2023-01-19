f9cfd3d44a8e96da7021f90d4742c629109010f8
ersakantibelva <ersakantibelva@gmail.com>
2023-01-19 23:14:54 +0700

    fix/resume page

--- my-porto-web/src/views/HomePage.js
+++ my-porto-web/src/views/HomePage.js
@@ -6,15 +6,15 @@ export default function HomePage() {
       <section class="bg-landing bg-no-repeat bg-cover bg-center bg-fixed h-full">
         <div class="backdrop-brightness-25 h-full">
           <div class="mx-auto container max-w-screen-xl px-4 py-32 lg:flex lg:items-center h-full">
-            <div className="flex flex-col justify-center h-full max-w-4xl mx-auto text-center 2xl:max-w-6xl">
-              <div className="text-4xl font-semibold md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-9xl">
+            <div className="flex flex-col justify-center h-full max-w-4xl mx-auto text-center lg:text-left 2xl:max-w-6xl">
+              <div className="text-4xl font-semibold md:text-5xl lg:text-5xl xl:text-7xl 2xl:text-9xl">
                 <span className="text-powder-blue">Hi,</span>
                 <span className="text-imperial-red"> I'am Belva</span>
               </div>
-              <div className="mt-2 text-2xl md:w-1/2 md:mx-auto text-powder-blue lg:text-4xl lg:w-3/5 2xl:text-7xl 2xl:w-full">
+              <div className="mt-2 text-2xl md:w-1/2 md:mx-auto text-powder-blue lg:text-3xl lg:w-full 2xl:text-7xl 2xl:w-full">
                 a female dev, a full-stack developer, and an IT ethusiast.
               </div>
-              <div className="flex flex-col w-3/4 gap-4 mx-auto mt-6 text-center 2xl:gap-6 md:justify-center md:flex-row flex-start lg:text-lg xl:text-xl 2xl:text-4xl">
+              <div className="flex flex-col w-3/4 gap-4 mx-auto mt-6 text-center lg:justify-start lg:w-full 2xl:gap-6 md:justify-center md:flex-row flex-start lg:text-lg xl:text-xl 2xl:text-4xl">
                 <Link to="/portfolio">
                   <div className="px-6 py-2 font-semibold rounded-full 2xl:py-4 2xl:px-10 bg-powder-blue text-prussian-blue">
                     see my portfolios
--- my-porto-web/src/views/ResumePage.js
+++ my-porto-web/src/views/ResumePage.js
@@ -1,7 +1,24 @@
 import BackButton from "../components/BackButton";
 import profilePic from "../assets/profile-pic.jpg";
+import cvPdf from "../assets/CV_ErsakantiBelva.pdf"
 
 export default function ResumePage() {
+
+  const handleDownloadPdf = () => {
+    fetch('CV_ErsakantiBelva.pdf')
+    .then((res) => {
+       return res.blob()
+      })
+      .then(blob => {
+        const fileUrl = window.URL.createObjectURL(blob)
+
+        let alink = document.createElement('a')
+        alink.href = fileUrl
+        alink.download = 'CV_ErsakantiBelva.pdf'
+        alink.click()
+      })
+  }
+
   return (
     <div className="container flex flex-col min-h-[calc(100%-8rem)] mx-auto mb-4">
       <BackButton route={"/"} message={"Back to home"} />
@@ -24,31 +41,119 @@ export default function ResumePage() {
         <div className="text-sm font-medium text-center text-prussian-blue">
           (tinycalicocat)
         </div>
-        <div className="text-center text-celadon-blue">
-          A female developer with engineering education background.
-        </div>
+        <div className="text-center text-celadon-blue">Software Engineer</div>
 
         <div className="mt-8 text-xl font-semibold text-center text-prussian-blue">
           Overview
         </div>
-        <div className="max-w-2xl mx-auto text-center text-celadon-blue">
-          I am an undergraduate engineering student passionate and interested in
-          software development. I am joining Hacktiv8 Bootcamp as student of
-          Full-Stack JavaScript to enrich my technical or soft skills, such as
-          JavaScript as main programming language, developing server using
-          ExpressJs and developing client-side website using frameworks
-          (ReactJs, VueJs). Moreover, being head of a department in a previous
-          organization, I learned to work under pressure individually and as a
-          team. I also learned to have a high dedication and responsibility
-          towards my work. I hope to further develop my experience by working as
-          a software engineer in this new opportunity.
+        <div className="max-w-2xl px-3 mx-auto text-justify text-celadon-blue">
+          A female developer with an engineering degree who is passionate and
+          interested in software development and an IT enthusiast. I joined
+          Hacktiv8 Bootcamp as a student of Full-Stack JavaScript to enrich my
+          technical or soft skills, such as JavaScript as the main programming
+          language, developing servers using ExpressJs, and developing
+          client-side websites using front-end frameworks (ReactJs, VueJs).
+          Moreover, I learned to work under pressure individually and as part of
+          a team while I was a head of a department in a previous organization.
+          I also learned to have high dedication, responsibility towards my
+          work, and eagerness to learn new things. I hope to further develop my
+          experience by working as a software engineer in the future.
         </div>
 
+        {/* <button onClick={handleDownloadPdf}>
+          Get My CV
+        </button> */}
+
+        <a href={cvPdf} target="_blank" rel="noreferrer" className="px-3 py-2 my-3 font-medium rounded-full w-fit bg-powder-blue text-imperial-red">
+          Open My CV
+        </a>
+{/* 
         <div className="mt-8 text-xl font-semibold text-center text-prussian-blue">
           Education
         </div>
 
-        <div className="flex flex-col justify-center w-full max-w-2xl mt-3 font-medium text-prussian-blue">
+        <div className="flex flex-col w-full px-3 mt-3 font-medium text-left text-prussian-blue">
+          <div>Full-Stack JavaScript Student</div>
+          <div>Hacktiv8 Indonesia</div>
+          <div className="font-normal">Sept 2022 – Jan 2023 (Remote)</div>
+          <ul className="max-w-2xl mx-auto font-normal text-celadon-blue">
+            <li>
+              - Learned programming and software development fundamentals
+            </li>
+            <li>
+              - Developed websites through projects assignments both server-side and client-side.{" "}
+            </li>
+          </ul>
+          <div>Awards: Teacher's Awards</div>
+          <div>Final Grade: 96.8%</div>
+          <div className="flex gap-3">
+            <a href="https://drive.google.com/file/d/1dqybouX8OBWgA_1bK2qHKPIrCcPMgYgQ/view?usp=sharing" target="_blank" className="cursor-pointer hover:text-celadon-blue text-imperial-red">
+              Certificate
+            </a>
+            <a href="https://drive.google.com/file/d/12v7qeItZ7dn0F2dqbdx_2swSWHfqXmHJ/view?usp=sharing" target="_blank" className="cursor-pointer hover:text-celadon-blue text-imperial-red">
+              Transcript
+            </a>
+          </div>
+        </div>
+
+        <div className="flex flex-col w-full px-3 mt-3 font-medium text-left text-prussian-blue">
+          <div>React Js Web Frontend Student</div>
+          <div>Sanbercode</div>
+          <div className="font-normal">Aug 2022 - Sept 2022 (Remote)</div>
+          <ul className="max-w-2xl mx-auto font-normal text-celadon-blue">
+            <li>
+              - Learned developing client-side website using React Js
+            </li>
+            <li>
+              - Developed website using authentication and deployment with Netlify{" "}
+            </li>
+          </ul>
+            <a href="https://drive.google.com/file/d/1ZUxSkvAWYIptawVUTL6mQNfDwJ8qephL/view?usp=sharing" target="_blank" className="cursor-pointer hover:text-celadon-blue text-imperial-red">
+              Certificate
+            </a>
+        </div>
+
+        <div className="flex flex-col w-full px-3 mt-3 font-medium text-left text-prussian-blue">
+          <div>Data Analyst Track Participant</div>
+          <div>Generasi GIGIH 2.0 by GoTo</div>
+          <div className="font-normal">Mar 2022 – Sept 2022 (Remote)</div>
+          <ul className="max-w-2xl mx-auto font-normal text-celadon-blue">
+            <li>
+              - Learned about Data Ecosystem, Data Analytic, Statistics, Data Processing using SQL, Python Programming (Numpy and Pandas), and Data Visualization with Google Data Studio.  
+            </li>
+            <li>
+              - Developed soft skills such as Adaptability, Time Management, Constructive Feedback, Critical Thinking, Life Path, Problem Solving, etc.
+            </li>
+            <li>
+              - Worked on a Capstone Project that collaborates with stakeholder and is fully engaged as a part of the engineering team.
+            </li>
+          </ul>
+            <a href="https://drive.google.com/drive/folders/1iETs3f55XcdbcZchJq3b1jJxFWDnXR1X?usp=sharing" target="_blank" className="cursor-pointer hover:text-celadon-blue text-imperial-red">
+              Certificate
+            </a>
+        </div>
+
+        <div className="flex flex-col w-full px-3 mt-3 font-medium text-left text-prussian-blue">
+          <div>Undergraduate, Engineering Physics</div>
+          <div>Sepuluh Nopember Institute of Technology</div>
+          <div className="font-normal">Jul 2018 – August 2022 (Surabaya)</div>
+          <ul className="max-w-2xl mx-auto font-normal text-celadon-blue">
+            <li>
+              <div className="font-semibold">Relevant Coursework:</div>
+              Computer Programming  
+            </li>
+            <li className="flex gap-1">
+              <div className="font-semibold">GPA: </div>
+              <div>3.63/4.00</div>
+            </li>
+            <li>
+              <div className="font-semibold">Final Project:</div>
+              Type-2 Fuzzy Logic System Model Design for Determination of M-Learning Usage Behavior Based On The Self-Efficacy and Facilitating Conditions Room Lighting Facility (Score: A)
+            </li>
+          </ul>
+        </div> */}
+
+        {/* <div className="flex flex-col justify-center w-full max-w-2xl mt-3 font-medium text-prussian-blue">
           <div className="flex justify-between">
             <h1>Full-Stack JavaScript Student</h1>
             <h1>Remote</h1>
@@ -69,9 +174,9 @@ export default function ResumePage() {
             - Develop 3 websites through projects assignment both in server-side
             and client-side.{" "}
           </li>
-        </ul>
+        </ul> */}
 
-        <div className="flex flex-col justify-center w-full max-w-2xl mt-3 font-medium text-prussian-blue">
+        {/* <div className="flex flex-col justify-center w-full max-w-2xl mt-3 font-medium text-prussian-blue">
           <div className="flex justify-between">
             <h1>Data Analyst Track Participant</h1>
             <h1>Remote</h1>
@@ -110,13 +215,64 @@ export default function ResumePage() {
         </div>
         <ul className="max-w-2xl mx-auto text-celadon-blue">
           <li>Relevant Coursework : Computer Programming </li>
-        </ul>
+        </ul> */}
 
-        <div className="mt-8 text-xl font-semibold text-center text-prussian-blue">
+        {/* <div className="mt-8 text-xl font-semibold text-center text-prussian-blue">
           Experience
         </div>
 
-        <div className="flex flex-col justify-center w-full max-w-2xl mt-3 font-medium text-prussian-blue">
+        <div className="flex flex-col w-full px-3 mt-3 font-medium text-left text-prussian-blue">
+          <div>Data Analyst Intern</div>
+          <div>PT Gizi Sehat Indonesia (Dietela.id)</div>
+          <div className="font-normal">May 2022 – August 2022 (Remote)</div>
+          <ul className="max-w-2xl mx-auto font-normal text-celadon-blue">
+            <li>
+              - Successfully analyzed over 400 Dietela’s clients and created the dashboard to get an overview of client demographics and personas using Google Data Studio and Python with Pandas 
+            </li>
+            <li>
+              - Accomplished to use Natural Language Processing (NLP) to process the data with text type using Sastrawi library
+            </li>
+          </ul>
+        </div>
+
+        <div className="flex flex-col w-full px-3 mt-3 font-medium text-left text-prussian-blue">
+          <div>Head of Communication and Information Department</div>
+          <div>Himpunan Mahasiswa Teknik Fisika, ITS</div>
+          <div className="font-normal">Nov 2020 – Dec 2021 (Surabaya)</div>
+          <ul className="max-w-2xl mx-auto font-normal text-celadon-blue">
+            <li>
+              - Responsible for conceptualizing 5 departmental work programs that run for 10 months. 
+            </li>
+            <li>
+              - Succeeded in creating a social media content scheduling management system for Instagram and LINE Official Accounts.
+            </li>
+            <li>
+              - Rewarded as one of the Best Departments in HMTF ITS 2020/2021
+            </li>
+          </ul>
+        </div>
+
+        <div className="flex flex-col w-full px-3 mt-3 font-medium text-left text-prussian-blue">
+          <div>Staff of Brand and Information Management</div>
+          <div>AIESEC in Surabaya</div>
+          <div className="font-normal">Feb 2021 – Jan 2022 (Surabaya)</div>
+          <ul className="max-w-2xl mx-auto font-normal text-celadon-blue">
+            <li>
+              - Produced branding content of AIESEC in Surabaya on Instagram and LinkedIn.
+            </li>
+            <li>
+              - Awarded the title of Member of The Month in February and March for the Staff category. 
+            </li>
+            <li>
+              - Succeeded in drafting more than 20 contents in a month. 
+            </li>
+            <li>
+              - Enabled to get a 25% increase in followers for 5 months. 
+            </li>
+          </ul>
+        </div> */}
+
+        {/* <div className="flex flex-col justify-center w-full max-w-2xl mt-3 font-medium text-prussian-blue">
           <div className="flex justify-between">
             <h1>Data Analyst Intern</h1>
             <h1>Remote</h1>
@@ -181,13 +337,13 @@ export default function ResumePage() {
           </li>
           <li>- Successfully drafted more than 20 content in a month.</li>
           <li>- Enabled to get a 25% increase in followers for 5 months. </li>
-        </ul>
+        </ul> */}
 
-        <div className="mt-8 text-xl font-semibold text-center text-prussian-blue">
-          Skills and Certification
-        </div>
+        {/* <div className="mt-8 text-xl font-semibold text-center text-prussian-blue">
+          Technical Skills
+        </div> */}
 
-        <ul className="max-w-2xl mx-auto text-celadon-blue">
+        {/* <ul className="max-w-2xl mx-auto text-celadon-blue">
           <li>
             <span className="font-semibold text-prussian-blue">
               Technical:{" "}
@@ -213,7 +369,7 @@ export default function ResumePage() {
             <span className="font-semibold text-prussian-blue">Language: </span>
             Indonesia (Native), English (Intermediate){" "}
           </li>
-        </ul>
+        </ul> */}
       </div>
     </div>
   );
